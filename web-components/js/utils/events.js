6c6d733706122fb1eb51e1df57354b5fc7805ae6
nabinchaulagain <wrighteric002@gmail.com>
2021-03-16 15:29:51 +0545

    empty master

--- web-components/js/utils/events.js
+++
@@ -1,8 +0,0 @@
-/**
- * return a custom addTodo event
- * @param {string} todo - todo text
- * @returns {CustomEvent} custom addTodo event with todo object in detail key of event
- */
-const makeTodoAddEvent = (todo) => {
-  return new CustomEvent('addTodo', { detail: { todo } });
-};
